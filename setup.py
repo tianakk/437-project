d244475d330a180ccaa00d7245544b3e69729b25
yousseffrk <youssef.mwalidfarouk@mail.mcgill.ca>
2022-12-06 18:27:27 -0500

    setup.py added

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='437-project',
+    version='1.0',
+    packages=['tests'],
+    url='',
+    license='',
+    author='yousseffarouk',
+    author_email='',
+    description='',
+    project_urls={
+        'Source': 'https://github.com/tianakk/437-project',
+    },
+    install_requires=['pytest', 'pytest-cov', 'prettytable'],
+    python_requires='>=3',
+)
