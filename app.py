8c776aef3820bd85abb31b8a3e749a299a17db96
tianakk <tianal.dem@gmail.com>
2022-12-06 21:14:45 -0500

    building application

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #source : https://medium.com/bettercode/how-to-build-a-modern-ci-cd-pipeline-5faa01891a5b
-import os
-from flask import Flask
+
+from flask import Flask, request, render_template
 import DriverClass
 
 app = Flask(__name__)
@@ -8,9 +8,15 @@ app = Flask(__name__)
 @app.route("/")
 def generate_buzz():
 
-    return 'Hello world'
-
+        # return DriverClass.mainMenu()
+        # page = '<html><body><h1>'
+        # page += DriverClass.mainMenu()
+        # page += '</h1></body></html>'
+        # return page
+        return '</h1></body></html>'
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
-
+        from waitress import serve
+        serve(app, host="0.0.0.0", port=8080)
+        # # return "<h1>DriverClass.mainMenu()<h1>"
+        # # return DriverClass.mainMenu
