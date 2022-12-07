205c1b3995baa61f5d4c4e8d95235cf5cfa172f6
yousseffrk <youssef.mwalidfarouk@mail.mcgill.ca>
2022-12-06 19:44:22 -0500

    Edited setup description

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import setup
 
 setup(
     name='437-project',
-    version='1.0',
+    version='1.1',
     packages=['tests'],
     url='https://github.com/tianakk/437-project',
     license='Personal',
     author='yousseffarouk',
     author_email='mail@mail.com',
-    description='desc',
-    long_description='desc',
+    description='Event management system',
+    long_description='Using this event management system, a user can create events or book tickets for am event as '
+                     'well as view all events and all tickets or get a summary of all tickets',
     project_urls={
         'Source': 'https://github.com/tianakk/437-project',
     },
